024c1d47534bcde208fb384c85c562b851a72d9b
LisJustino <lisjustino@gmail.com>
2026-08-18 00:10:45 -0300

    feat: conclui landing page publica

---
+++ js/pages/landing.js
@@ -0,0 +1,34 @@
+/**
+ * ==========================================================================
+ * Visium
+ * Arquivo: landing.js
+ *
+ * Comportamento da Landing Page.
+ * ==========================================================================
+ */
+
+"use strict";
+
+
+/* ==========================================================================
+   Inicialização
+========================================================================== */
+
+function initializeLanding() {
+
+    /*
+     * A Landing atualmente não depende de lógica dinâmica.
+     * Este ponto existe para centralizar futuras inicializações.
+     */
+
+    console.log(
+        "Visium | Landing Page inicializada."
+    );
+
+}
+
+
+document.addEventListener(
+    "DOMContentLoaded",
+    initializeLanding
+);
